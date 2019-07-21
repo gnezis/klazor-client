8f33c6bf1d86ad6552e4392c7129957114535ab2
Wilfried Bounsi <wilcoln99@gmail.com>
2019-07-21 12:37:43 +0200

    Fix small error

--- klazor_client/client.py
+++ klazor_client/client.py
@@ -14,40 +14,43 @@ def config(**kwargs):
                 settings.PASSWORD = value
 
 
-auth_values = (settings.LOGIN, settings.PASSWORD)
-
-
 def fetch_courses():
     courses_url = settings.API_URL + '/course/'
+    auth_values = (settings.LOGIN, settings.PASSWORD)
     response = requests.get(courses_url, auth=auth_values)
     return utils.courses_from_dict(response.json())
 
 
 def fetch_sheets():
     sheets_url = settings.API_URL + '/sheet/'
+    auth_values = (settings.LOGIN, settings.PASSWORD)
     response = requests.get(sheets_url, auth=auth_values)
     return utils.sheets_from_dict(response.json())
 
 
 def fetch_course(id):
     course_url = settings.API_URL + '/course/' + str(id)
+    auth_values = (settings.LOGIN, settings.PASSWORD)
     response = requests.get(course_url, auth=auth_values)
     return utils.course_from_dict(response.json())
 
 
 def fetch_sheet(id):
     sheet_url = settings.API_URL + '/sheet/' + str(id)
+    auth_values = (settings.LOGIN, settings.PASSWORD)
     response = requests.get(sheet_url, auth=auth_values)
     return utils.sheet_from_dict(response.json())
 
 
 def fetch_instructor(id):
     instructor_url = settings.API_URL + '/instructor/' + str(id)
+    auth_values = (settings.LOGIN, settings.PASSWORD)
     response = requests.get(instructor_url, auth=auth_values)
     return utils.instructor_from_dict(response.json())
 
 
 def fetch_instructors():
     instructor_url = settings.API_URL + '/instructor/'
+    auth_values = (settings.LOGIN, settings.PASSWORD)
     response = requests.get(instructor_url, auth=auth_values)
     return utils.instructors_from_dict(response.json())
